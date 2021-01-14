12f3587d25bbe7d982276a262d88e197df929405
Kydroid <yalmahdaoui@gmail.com>
2021-01-14 20:57:08 +0100

    Algorithm (use TDD style baby-step) : Get optimized change from payment. Adding tests between $0 and $26 without refacto to bring out the algorithm.

---
+++ ChangeFromAutomaticCheckout/src/main/java/org/example/Change.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Change {
+    public static final int VALUE_BILL_10 = 10;
+    public static final int VALUE_BILL_5 = 5;
+    public static final int VALUE_COIN_2 = 2;
+
+    private int numBill10;
+    private int numBill5;
+    private int numCoin2;
+
+    public Change() {
+    }
+
+    public Change(int numBill10, int numBill5, int numCoin2) {
+        this.numBill10 = numBill10;
+        this.numBill5 = numBill5;
+        this.numCoin2 = numCoin2;
+    }
+
+    public int getNumBill10() {
+        return numBill10;
+    }
+
+    public void setNumBill10(int numBill10) {
+        this.numBill10 = numBill10;
+    }
+
+    public int getNumBill5() {
+        return numBill5;
+    }
+
+    public void setNumBill5(int numBill5) {
+        this.numBill5 = numBill5;
+    }
+
+    public int getNumCoin2() {
+        return numCoin2;
+    }
+
+    public void setNumCoin2(int numCoin2) {
+        this.numCoin2 = numCoin2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Change change = (Change) o;
+        return numBill10 == change.numBill10 &&
+                numBill5 == change.numBill5 &&
+                numCoin2 == change.numCoin2;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numBill10, numBill5, numCoin2);
+    }
+
+    @Override
+    public String toString() {
+        return "Change{" +
+                "numBill10=" + numBill10 +
+                ", numBill5=" + numBill5 +
+                ", numCoin2=" + numCoin2 +
+                '}';
+    }
+}
---
+++ ChangeFromAutomaticCheckout/src/test/java/org/example/AppTest.java
@@ -0,0 +1,97 @@
+package org.example;
+
+//import static org.junit.Assert.assertTrue;
+import static org.example.App.calculateChange;
+import static org.junit.jupiter.api.Assertions.*;
+
+//import org.junit.Test;
+import org.junit.jupiter.api.*;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest
+{
+
+    @Test
+    void throwException_whenCalculateChangeIsImpossible() {
+        assertThrows(Exception.class, () -> calculateChange(0));
+        assertThrows(Exception.class, () -> calculateChange(1));
+        assertThrows(Exception.class, () -> calculateChange(3));
+    }
+
+    @Test
+    void returnChange_whenCalculateChangeIsPossible() throws Exception {
+        Change changeExpectedFor2 = new Change(0, 0, 1);
+        assertEquals(changeExpectedFor2, calculateChange(2));
+
+        Change changeExpectedFor4 = new Change(0, 0, 2);
+        assertEquals(changeExpectedFor4, calculateChange(4));
+
+        Change changeExpectedFor5 = new Change(0, 1, 0);
+        assertEquals(changeExpectedFor5, calculateChange(5));
+
+        Change changeExpectedFor6 = new Change(0, 0, 3);
+        assertEquals(changeExpectedFor6, calculateChange(6));
+
+        Change changeExpectedFor7 = new Change(0, 1, 1);
+        assertEquals(changeExpectedFor7, calculateChange(7));
+
+        Change changeExpectedFor8 = new Change(0, 0, 4);
+        assertEquals(changeExpectedFor8, calculateChange(8));
+
+        Change changeExpectedFor9 = new Change(0, 1, 2);
+        assertEquals(changeExpectedFor9, calculateChange(9));
+
+        Change changeExpectedFor10 = new Change(1, 0, 0);
+        assertEquals(changeExpectedFor10, calculateChange(10));
+
+        Change changeExpectedFor11 = new Change(0, 1, 3);
+        assertEquals(changeExpectedFor11, calculateChange(11));
+
+        Change changeExpectedFor12 = new Change(1, 0, 1);
+        assertEquals(changeExpectedFor12, calculateChange(12));
+
+        Change changeExpectedFor13 = new Change(0, 1, 4);
+        assertEquals(changeExpectedFor13, calculateChange(13));
+
+        Change changeExpectedFor14 = new Change(1, 0, 2);
+        assertEquals(changeExpectedFor14, calculateChange(14));
+
+        Change changeExpectedFor15 = new Change(1, 1, 0);
+        assertEquals(changeExpectedFor15, calculateChange(15));
+
+        Change changeExpectedFor16 = new Change(1, 0, 3);
+        assertEquals(changeExpectedFor16, calculateChange(16));
+
+        Change changeExpectedFor17 = new Change(1, 1, 1);
+        assertEquals(changeExpectedFor17, calculateChange(17));
+
+        Change changeExpectedFor18 = new Change(1, 0, 4);
+        assertEquals(changeExpectedFor18, calculateChange(18));
+
+        Change changeExpectedFor19 = new Change(1, 1, 2);
+        assertEquals(changeExpectedFor19, calculateChange(19));
+
+        Change changeExpectedFor20 = new Change(2, 0, 0);
+        assertEquals(changeExpectedFor20, calculateChange(20));
+
+        Change changeExpectedFor21 = new Change(1, 1, 3);
+        assertEquals(changeExpectedFor21, calculateChange(21));
+
+        Change changeExpectedFor22 = new Change(2, 0, 1);
+        assertEquals(changeExpectedFor22, calculateChange(22));
+
+        Change changeExpectedFor23 = new Change(1, 1, 4);
+        assertEquals(changeExpectedFor23, calculateChange(23));
+
+        Change changeExpectedFor24 = new Change(2, 0, 2);
+        assertEquals(changeExpectedFor24, calculateChange(24));
+
+        Change changeExpectedFor25 = new Change(2, 1, 0);
+        assertEquals(changeExpectedFor25, calculateChange(25));
+
+        Change changeExpectedFor26 = new Change(2, 0, 3);
+        assertEquals(changeExpectedFor26, calculateChange(26));
+    }
+}

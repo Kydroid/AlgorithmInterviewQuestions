c3183dbdd9ba8da14a90343d10599211ae563331
Kydroid <yalmahdaoui@gmail.com>
2021-01-20 20:07:56 +0100

    Algorithm (use TDD style baby-step) : Reverse order of a list. Commit step by step for the example. Step 8 (final) refacto

--- Algorithms/src/main/java/org/example/reverselist/ReverseArray.java
+++ Algorithms/src/main/java/org/example/reverselist/ReverseArray.java
@@ -5,12 +5,16 @@ public class ReverseArray {
 
     public static int[] reverseOrder(int[] arrayToReverse) {
         if (arrayToReverse != null && arrayToReverse.length > 1) {
-            for (int i = 0; i < arrayToReverse.length/2; i++) {
-                int elementToSwitch = arrayToReverse[i];
-                arrayToReverse[i] = arrayToReverse[arrayToReverse.length-1-i];
-                arrayToReverse[arrayToReverse.length-1-i] = elementToSwitch;
+            for (int i = 0; i < arrayToReverse.length / 2; i++) {
+                switchElements(arrayToReverse, i, arrayToReverse.length - 1 - i);
             }
         }
         return arrayToReverse;
+    }
+
+    private static void switchElements(int[] array, int indexElementA, int indexElementB) {
+        int elementToSwitch = array[indexElementA];
+        array[indexElementA] = array[indexElementB];
+        array[indexElementB] = elementToSwitch;
     }
 }

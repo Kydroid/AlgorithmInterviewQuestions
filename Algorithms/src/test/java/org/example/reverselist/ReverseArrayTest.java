03f69416177b1b1552f344a99c158dd5d9be6b52
Kydroid <yalmahdaoui@gmail.com>
2021-01-20 20:01:24 +0100

    Algorithm (use TDD style baby-step) : Reverse order of a list. Commit step by step for the example. Step 7

--- Algorithms/src/test/java/org/example/reverselist/ReverseArrayTest.java
+++ Algorithms/src/test/java/org/example/reverselist/ReverseArrayTest.java
@@ -36,6 +36,14 @@ class ReverseArrayTest {
         array = new int[]{1, 2, 3, 4};
         reverseArrayExpected = new int[]{4, 3, 2, 1};
         assertArrayEquals(reverseArrayExpected, ReverseArray.reverseOrder(array));
+
+        array = new int[]{1, 2, 3, 4, 5};
+        reverseArrayExpected = new int[]{5, 4, 3, 2, 1};
+        assertArrayEquals(reverseArrayExpected, ReverseArray.reverseOrder(array));
+
+        array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+        reverseArrayExpected = new int[]{19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        assertArrayEquals(reverseArrayExpected, ReverseArray.reverseOrder(array));
     }
 
 }
